60fc06f09d1479e69c4b3fe692f7704a2661bdc0
Fran McDade <franmcdade@Frans-MBP.hub>
2021-03-12 22:16:11 +1000

    Dataset attributes spreadsheet cleanup. #929.

--- plugins/gatsby-source-dashboard-anvil/gatsby-node.js
+++ plugins/gatsby-source-dashboard-anvil/gatsby-node.js
@@ -9,19 +9,74 @@ const path = require("path");
 
 // App dependencies
 const {getFieldTypeWorkspaceAccessType, getFieldTypeWorkspaceDiseases, getFieldTypeWorkspaceGapId, getFieldTypeWorkspaceStudyName} = require(path.resolve(__dirname, "../utils/dashboard-field-extension-anvil.service.js"));
-const {generateAnVILDashboardIndex} = require(path.resolve(__dirname, "../utils/dashboard-indexing-anvil.service.js"));
+const {generateAnVILIngestedDashboardIndex} = require(path.resolve(__dirname, "../utils/dashboard-indexing-ingested.service.js"));
+// const {generateAnVILDashboardIndex} = require(path.resolve(__dirname, "../utils/dashboard-indexing-anvil.service.js"));
 const {getStudies} = require(path.resolve(__dirname, "../utils/dashboard-studies-anvil.service.js"));
+const {getNCBIStudies} = require(path.resolve(__dirname, "../utils/dashboard-studies-ingested.service.js"));
 const {getWorkspaces} = require(path.resolve(__dirname, "../utils/dashboard-workspaces-anvil.service.js"));
+const {getIngestedWorkspaces} = require(path.resolve(__dirname, "../utils/dashboard-workspaces-ingested.js"));
 
 exports.sourceNodes = async ({actions, createNodeId, createContentDigest}) => {
 
     const {createNode} = actions;
+
+    /* Build up the workspaces model from the ingested attributes. */
+    const ingestedWorkspaces = await getIngestedWorkspaces();
+
+    /* Build up the studies model from the ingested workspaces. */
+    const ingestedStudies = await getNCBIStudies(ingestedWorkspaces);
 
     /* Build up the workspaces model. */
     const workspaces = await getWorkspaces();
 
     /* Build up the studies model. */
     const studies = await getStudies(workspaces);
+
+    /* Create node - ingested studies. */
+    ingestedStudies.forEach(study => {
+
+        const nodeContent = JSON.stringify(study);
+        const studyId = `${study.dbGapId}${study.studyName}`;
+
+        const nodeMeta = {
+            id: createNodeId(studyId),
+            parent: null,
+            children: [],
+            internal: {
+                type: `IngestedStudy`,
+                mediaType: `application/json`,
+                content: nodeContent,
+                contentDigest: createContentDigest(study),
+            },
+        };
+
+        const node = Object.assign({}, study, nodeMeta);
+
+        createNode(node)
+    });
+
+    /* Create node - ingested workspaces. */
+    ingestedWorkspaces.forEach(workspace => {
+
+        const nodeContent = JSON.stringify(workspace);
+        const workspaceId = `${workspace.consortium}${workspace.projectId}`;
+
+        const nodeMeta = {
+            id: createNodeId(workspaceId),
+            parent: null,
+            children: [],
+            internal: {
+                type: `IngestedWorkspace`,
+                mediaType: `application/json`,
+                content: nodeContent,
+                contentDigest: createContentDigest(workspace),
+            },
+        };
+
+        const node = Object.assign({}, workspace, nodeMeta);
+
+        createNode(node)
+    });
 
     /* Create node - study. */
     studies.forEach(study => {
@@ -110,6 +165,30 @@ exports.createSchemaCustomization = ({actions}) => {
     });
 
     createFieldExtension({
+        name: "iGapId",
+        extend(options, prevFieldConfig) {
+            return {
+                resolve(source, arg, context, info) {
+                    const studies = context.nodeModel.getAllNodes({type: "IngestedStudy"});
+                    return getFieldTypeWorkspaceGapId(source, studies);
+                },
+            }
+        }
+    });
+
+    createFieldExtension({
+        name: "iStudyName",
+        extend(options, prevFieldConfig) {
+            return {
+                resolve(source, arg, context, info) {
+                    const studies = context.nodeModel.getAllNodes({type: "IngestedStudy"});
+                    return getFieldTypeWorkspaceStudyName(source, studies);
+                },
+            }
+        }
+    });
+
+    createFieldExtension({
         name: "studyName",
         extend(options, prevFieldConfig) {
             return {
@@ -136,6 +215,33 @@ exports.createSchemaCustomization = ({actions}) => {
     type GapId implements Node {
         gapIdDisplay: String
         studyUrl: String
+    }
+    type IngestedStudy implements Node {
+        id: ID!
+        consentGroup: ConsentGroup
+        dbGapIdAccession: String
+        diseases: [String]
+        gapId: GapId
+        studyName: String
+        studyUrl: String
+        subjectsCount: Int
+        subjectsTotal: Int
+    }
+    type IngestedWorkspace implements Node {
+        id: ID!
+        accessType: String
+        consentShortNames: [String]
+        consortium: String
+        dataTypes: [String]
+        dbGapId: String
+        dbGapIdAccession: String
+        diseases: [String]
+        gapId: GapId @iGapId
+        projectId: String
+        samples: Int
+        size: Float
+        studyName: String @iStudyName
+        subjects: Int
     }
     type Workspace implements Node {
         id: ID!
@@ -204,11 +310,11 @@ exports.createResolvers = ({createResolvers}) => {
 exports.onPostBootstrap = ({getNodesByType}) => {
 
     /* Get the workspaces. */
-    const workspaces = getNodesByType("Workspace");
+    const workspaces = getNodesByType("IngestedWorkspace");
 
     /* Get the studies. */
-    const studies = getNodesByType("Study");
+    const studies = getNodesByType("IngestedStudy");
 
-    /* Generate the dashboard search index. */
-    generateAnVILDashboardIndex(studies, workspaces);
+    /* Generate the ingested dashboard search index. */
+    generateAnVILIngestedDashboardIndex(studies, workspaces);
 };
